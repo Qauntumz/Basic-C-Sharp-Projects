4c6578c4ed84ff4a460371bf26436cf15310c46c
Josh Smith <purejosh12@gmail.com>
2020-04-14 08:38:40 -0600

    aseras

---
+++ Car Insurance approval/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Car_Insurance_approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool InsuranceApproved = (age > 15 && dui == "no" && tickets < 3);
+
+            Console.WriteLine("Are you approved for insurance? " + InsuranceApproved);
+        }
+    }
+}
