ab1ba44562263a41c215030f6ce7d484c6ecba7e
Josh Smith <purejosh12@gmail.com>
2020-04-15 03:01:24 -0600

    asdf

---
+++ shipping quote package/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace shipping_quote_package
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight.");
+
+            decimal pkgwght = Convert.ToDecimal(Console.ReadLine());
+            if (pkgwght > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+
+                Console.WriteLine("Please enter the package width.");
+                decimal pkgwidth = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height.");
+                decimal pkgheight = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length.");
+                decimal pkglength = Convert.ToDecimal(Console.ReadLine());
+
+                if ((pkgwidth + pkgheight + pkglength) > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                }
+                else
+                {
+                    decimal totalcost = (pkgwidth * pkgheight * pkglength * pkgwght) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + totalcost +".00");
+                }
+            }
+        }
+    }
+}
