9dfbf6860ad4d6167905777eed29cc741b8da672
Josh Smith <purejosh12@gmail.com>
2020-04-16 09:28:59 -0600

    yase

---
+++ concat toupper strinbuilder page131/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace concat_toupper_strinbuilder_page131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello ";
+            string b = "how ";
+            string c = "are you";
+            string all = a + b + c;
+            Console.WriteLine(all);
+            string d = all.ToUpper();
+            Console.WriteLine(d);
+
+            StringBuilder sb = new StringBuilder("Hello World!", 25);
+            sb.Append(" How are you today?");
+            sb.Append(" I hope you are having a wonderful day working on your schoolwork for the tech academy c# course.");
+            sb.Append(" Stay safe!!");
+            Console.WriteLine(sb);
+        }
+    }
+}
