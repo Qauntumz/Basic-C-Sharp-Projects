3b49bf3a4a2d12a90777576236ef0eee3de63019
Qauntumz <60720585+Qauntumz@users.noreply.github.com>
2020-04-19 05:57:34 -0700

    xzcvzxcvzxdf

---
+++ methods pg 160/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_pg_160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter twp values to add them to eachother, or one value to add 2 to it.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            mathop op = new mathop();
+
+            Console.WriteLine(op.MathOperation(x, y));
+
+            
+        }
+    }
+}
---
+++ methods pg 160/mathop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_pg_160
+{
+    class mathop
+    {
+        public int MathOperation(int x, int y = 2)
+        {
+            return x + y;
+        }
+    }
+}
