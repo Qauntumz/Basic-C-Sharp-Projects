3c77e3badc0a896e0a06f5e422b84d1c19ee435b
Qauntumz <60720585+Qauntumz@users.noreply.github.com>
2020-04-19 04:06:05 -0700

    methods finished

---
+++ class assignment pg156/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_assignment_pg156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Choose two values to multiply together");
+
+                mathop op = new mathop();
+
+                op.Multiply();
+
+                Console.WriteLine("Choose two values to divide the first by the second");
+                op.Divide();
+
+                Console.WriteLine("Choose two values to subtract the first by the second");
+                op.Subtract();
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ class assignment pg156/mathop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_assignment_pg156
+{
+    public class mathop
+    {
+        public void Multiply()
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+            int Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(X * Y);
+           
+        }
+        public void Divide()
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+            int Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(X / Y);
+
+        }
+        public void Subtract()
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+            int Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(X - Y);
+
+        }
+        public int Y { get; }
+        public int X { get; }
+    }
+}
