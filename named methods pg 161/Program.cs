0b6462b5de9e583c332807e654c6a0745d92233b
Qauntumz <60720585+Qauntumz@users.noreply.github.com>
2020-04-19 21:33:08 -0700

    named methods

---
+++ named methods pg 161/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace named_methods_pg_161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Named parameters");
+            mathop op = new mathop();
+
+            op.MathOperation(Weight: 32, finalPrice: 64);
+
+        }
+    }
+}
---
+++ named methods pg 161/mathop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace named_methods_pg_161
+{
+    class mathop
+    {
+        public void MathOperation(int Weight, int finalPrice)
+        {
+            Console.WriteLine("Weight = " + Weight + " * 2 to get final price");
+            Console.WriteLine("final price = " + finalPrice);
+        }
+    }
+}
