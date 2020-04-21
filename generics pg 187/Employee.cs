1eb1ea4acb01a3cb788600ff156d65035385b12e
Qauntumz <60720585+Qauntumz@users.noreply.github.com>
2020-04-21 04:22:55 -0700

    gen 187

---
+++ generics pg 187/Employee.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_overload_184
+{
+    public class Employee<T> : Person, Iquittable
+    {
+        public override void sayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("I Quit");
+        }
+        public List<T> Things { get; set; }
+
+        /*        public override string ToString()
+                {
+                    return base.ToString();
+                }
+
+                public override int GetHashCode()
+                {
+                    return ID;
+                }*/
+
+        public int ID { get; set; }
+
+/*        public override bool Equals(object obj)
+        {
+            Employee nployee = obj as Employee<string>;
+            if (object.ReferenceEquals(null, nployee)) return false;
+            return ID == nployee.ID;
+        }
+
+        public static bool operator ==(Employee<string> employee, Employee<string> nployee)
+        {
+            if (object.ReferenceEquals(null, employee))
+                return object.ReferenceEquals(null, nployee);
+            else if (object.ReferenceEquals(null, nployee))
+                return false;
+            return employee.Equals(nployee);
+        }
+
+        public static bool operator !=(Employee<string> employee, Employee<string> nployee)
+        {
+            if (object.ReferenceEquals(null, employee))
+                return !object.ReferenceEquals(null, nployee);
+            else if (object.ReferenceEquals(null, nployee))
+                return true;
+            return !employee.Equals(nployee);
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+        /*       public static bool operator== (Employee<string> employee, Employee nployee)
+               {
+                   if (employee.ID == nployee.ID)
+                   {
+                       return true;
+                   }
+
+               }
+               public static bool operator !=(Employee employee, Employee nployee)
+               {
+                   if (employee.ID != nployee.ID)
+                   {
+                       return employee != nployee;
+                   }
+
+               }*/
+    }
+}
---
+++ generics pg 187/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_overload_184
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iquittable mployee = new Employee<string>();
+            mployee.Quit();
+
+
+            Employee<string> employee = new Employee<string>();
+            employee.ID = 6;
+            Employee<string> nployee = new Employee<string>();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            nployee.ID = 6;
+            employee.sayName();
+            /*            Console.WriteLine(nployee == employee);*/
+
+
+            Employee<string> rployee = new Employee<string>();
+            rployee.Things = new List<string>() { "thing 1", "thing 2", "thing 3", "thing 4" };
+
+            Employee<int> kployee = new Employee<int>();
+            kployee.Things = new List<int>() { 1, 2, 3, 4};
+
+            foreach (string Things in rployee.Things)
+            {
+                Console.WriteLine(Things);
+            }
+            foreach (int Things in kployee.Things)
+            {
+                Console.WriteLine(Things);
+            }
+        }
+    }
+}
