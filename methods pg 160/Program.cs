2a0c8bd2ee78d8e77c84c68b51fdcf67e575b2b2
Josh Smith <purejosh12@gmail.com>
2020-04-21 10:58:08 -0600

    methods 160 update

--- methods pg 160/Program.cs
+++ methods pg 160/Program.cs
@@ -10,15 +10,50 @@ namespace methods_pg_160
     {
         static void Main(string[] args)
         {
+            try
+            {
+                mathop op = new mathop();
+                Console.WriteLine("Please enter two values to add them to eachother, or one value to add 2 to it.");
+                int x = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter twp values to add them to eachother, or one value to add 2 to it.");
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
-            mathop op = new mathop();
 
-            Console.WriteLine(op.MathOperation(x, y));
+                int y;
+                int asdf;
+                if(int.TryParse(Console.ReadLine(), out y))
+                {
+                    asdf = op.MathOperation(x, y);
+                }
+                else
+                {
+                    asdf = op.MathOperation(x);
+                }
+                Console.WriteLine(asdf);
 
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NotFiniteNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             
+            
+/*            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }*/
+
+
         }
     }
 }
