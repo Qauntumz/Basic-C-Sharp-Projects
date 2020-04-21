22265aa9e1f9cbd6fd0d8e274ee03783d2e7513f
Josh Smith <purejosh12@gmail.com>
2020-04-21 13:40:25 -0600

    ff

---
+++ struct 197/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_197
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 16;
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
