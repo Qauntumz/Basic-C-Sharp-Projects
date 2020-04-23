495aaa8974ec6769f6c819c5426e15cc4bf6c2f2
Josh Smith <purejosh12@gmail.com>
2020-04-23 07:29:24 -0600

    218

---
+++ file io pg 218/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace file_io_pg_218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\purej\Desktop\log2.txt", true))
+            {
+                file.WriteLine(x);
+            }
+            string y = File.ReadAllText(@"C:\Users\purej\Desktop\log2.txt");
+            Console.WriteLine(y);
+        }
+    }
+}
